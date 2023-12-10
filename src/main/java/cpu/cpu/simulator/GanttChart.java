b524225f9ba0227bdf64562310f76b3c221083a8
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2023-12-10 04:20:45 +0200

    change some functions in the ganttchart class

--- src/main/java/cpu/cpu/simulator/GanttChart.java
+++ src/main/java/cpu/cpu/simulator/GanttChart.java
@@ -23,69 +23,15 @@ public class GanttChart extends JPanel {
         setLayout(new BorderLayout());
     }
 
-    public void setDataset( String title, String x_label, String y_label) {
+    public void setDataset(IntervalCategoryDataset dataset, String title, String x_label, String y_label) {
         removeAll();
-        IntervalCategoryDataset dataset=getCategoryDataset();
         JFreeChart chart = ChartFactory.createGanttChart(title, y_label, x_label, dataset,false,false,false);
         ChartPanel panel = new ChartPanel(chart);
         add(panel);
         repaint();
         revalidate();
     }
-    private IntervalCategoryDataset getCategoryDataset() {  
-  
-        TaskSeries series1 = new TaskSeries("Estimated Date"); series1.add(new Task("Requirement",  
-        Date.from(LocalDate.of(2017,7,3).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-        Date.from(LocalDate.of(2017, 7,7).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series1.add(new Task("Design",Date.from(LocalDate.of(2017, 7,10).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 7, 14).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series1.add(new Task("Coding",Date.from(LocalDate.of(2017, 7,17).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 7, 21).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series1.add(new Task("Testing", Date.from(LocalDate.of(2017, 7,24).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 7, 28).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series1.add(new Task("Deployment", Date.from(LocalDate.of(2017, 07,31).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 8, 4).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-
-        TaskSeries series2 = new TaskSeries("Actual Date");  
-        series2.add(new Task("Requirement",Date.from(LocalDate.of(2017, 7,3).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-        Date.from(LocalDate.of(2017, 7, 05).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series2.add(new Task("Design",  
-         Date.from(LocalDate.of(2017, 7, 6).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 7, 17).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series2.add(new Task("Coding",  
-         Date.from(LocalDate.of(2017, 7, 18).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 7, 27).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series2.add(new Task("Testing",  
-         Date.from(LocalDate.of(2017, 7, 28).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 8, 1).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         series2.add(new Task("Deployment",  
-         Date.from(LocalDate.of(2017, 8, 2).atStartOfDay().toInstant(ZoneOffset.UTC)),  
-         Date.from(LocalDate.of(2017, 8, 4).atStartOfDay().toInstant(ZoneOffset.UTC))  
-                 ));  
-
-         TaskSeriesCollection dataset = new TaskSeriesCollection();  
-         dataset.add(series1);dataset.add(series2);  
-         return dataset;  
-    }
-} 
+}
     
   
   
